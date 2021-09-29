4a7ccfc16f2dead5014a9b1b27fd802907ffd19c
chiyongs <tlscldyd@gmail.com>
2021-09-29 19:30:29 +0900

    Add registerCorspattern for login

--- src/main/java/wiwy/covid/controller/MemberController.java
+++ src/main/java/wiwy/covid/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import wiwy.covid.config.auth.PrincipalDetails;
 import wiwy.covid.domain.Member;
+import wiwy.covid.repository.MemberRepository;
 import wiwy.covid.service.MemberService;
 
 
@@ -26,11 +27,17 @@ public class MemberController {
     public String join(@RequestBody Member member) {
         memberService.join(member);
         return "회원가입 완료";
+
     }
 
     @GetMapping("/api/member")
     public String member() {
         return "member";
+    }
+
+    @PostMapping("/api/login")
+    public String login(@RequestBody String username, @RequestBody String password) {
+        return "hi";
     }
 
 }
