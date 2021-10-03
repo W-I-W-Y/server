23cc7d87869c545e1086a34f800735424d66f0ae
chiyongs <tlscldyd@gmail.com>
2021-10-03 20:33:35 +0900

    Fix token expirationTime for develop FE

--- src/main/java/wiwy/covid/config/jwt/JwtProperties.java
+++ src/main/java/wiwy/covid/config/jwt/JwtProperties.java
@@ -2,7 +2,8 @@ package wiwy.covid.config.jwt;
 
 public interface JwtProperties {
     String SECRET = "wiwy";
-    int EXPIRATION_TIME = 60000*60; // 10분
+//    int EXPIRATION_TIME = 60000*60; // 10분
+    int EXPIRATION_TIME = 60000*60*6*24;
     String TOKEN_PREFIX ="Bearer ";
     String HEADER_STRING = "Authorization";
 }
