96b99e8b71e2906ba3d5f6e8831bc5ccd55d5749
chiyongs <tlscldyd@gmail.com>
2021-10-03 22:00:13 +0900

    Add postId in SmallPostDTO for viewing the post

--- src/main/java/wiwy/covid/domain/DTO/community/SmallPostDTO.java
+++ src/main/java/wiwy/covid/domain/DTO/community/SmallPostDTO.java
@@ -6,10 +6,12 @@ import wiwy.covid.domain.Post;
 @Getter
 public class SmallPostDTO {
 
+    private Long postId;
     private String calculateTime;
     private String postName;
 
     public SmallPostDTO(Post post) {
+        this.postId = post.getId();
         this.calculateTime = post.calculateTime(post.getCreateTime());
         this.postName = post.getPostName();
     }
